c83d62227c386551154ef712b9c91a588074790a
Thays Casado <thayscasado@hotmail.com>
2021-07-29 00:37:05 -0700

    App component using ChatEngine template

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { ChatEngine } from "react-chat-engine";
+
+// import App.css file
+import "./App.css";
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="a3e42231-f5ca-4aa0-8064-8087cc41ca99
+  "
+      userName="tcsd"
+      userSecret="123321"
+    />
+  );
+};
+
+export default App;
